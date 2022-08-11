c7e17dae38c2d16d4b2b511962ec835e074ef593
Pablo Martin Prediger <marpresi@gmail.com>
2022-08-11 20:52:55 -0300

    Clase 2 - Desafío: Menú e-commerce

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,42 +3,42 @@ class NavBar extends React.Component {
     render() {
       return <>
         <header>
-    <div class="px-3 py-2 text-bg-dark">
-      <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-          <i class="bi bi-bootstrap"></i> 
+    <div className="px-3 py-2 text-bg-dark">
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <a href="/" className="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+          <i className="bi bi-bootstrap"></i> 
           Tienda Prediger
           </a>
 
-          <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+          <ul className="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
             <li>
-              <a href="/home" class="nav-link text-secondary">
-              <i class="bi bi-house"></i> 
+              <a href="/home" className="nav-link text-secondary">
+              <i className="bi bi-house"></i> 
                 Home
               </a>
             </li>
             <li>
-              <a href="/dashboard" class="nav-link text-white">
-              <i class="bi bi-speedometer2"></i>
+              <a href="/dashboard" className="nav-link text-white">
+              <i className="bi bi-speedometer2"></i>
                 Dashboard
               </a>
             </li>
             <li>
-              <a href="/orders" class="nav-link text-white">
-              <i class="bi bi-table"></i>
+              <a href="/orders" className="nav-link text-white">
+              <i className="bi bi-table"></i>
                 Orders
               </a>
             </li>
             <li>
-              <a href="/products" class="nav-link text-white">
-              <i class="bi bi-grid"></i>
+              <a href="/products" className="nav-link text-white">
+              <i className="bi bi-grid"></i>
                 Products
               </a>
             </li>
             <li>
-              <a href="/customers" class="nav-link text-white">
-              <i class="bi bi-person-circle"></i>
+              <a href="/customers" className="nav-link text-white">
+              <i className="bi bi-person-circle"></i>
                 Customers
               </a>
             </li>
